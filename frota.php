99b7c6e41543f710ea4b82b0b897881b1f134b07
Eduardo Terra <Eduardo Terra>
2016-10-24 23:17:17 -0200

    alterando nomes da class Carros

--- frota.php
+++ frota.php
@@ -10,8 +10,8 @@
         <div id="columns">
           <div class="pin">
             <img src="dist/img/carros/Camaro.jpg" alt="" class="img" />
-            <h3 class="grupo">Grupo A</h3>
-            <p class="nome">Celta</p>
+            <h3 class="grupo">Grupo F</h3>
+            <p class="nome">Camaro</p>
           </div>
 
           <div class="pin">
@@ -22,98 +22,98 @@
 
           <div class="pin">
             <img src="dist/img/carros/civic.png" alt="" class="img" />
-            <h3 class="grupo">Grupo A</h3>
-            <p class="nome">Celta</p>
+            <h3 class="grupo">Grupo D</h3>
+            <p class="nome">Honda Civic</p>
           </div>
 
           <div class="pin">
             <img src="dist/img/carros/classic-sedan.jpg" alt="" class="img" />
             <h3 class="grupo">Grupo A</h3>
-            <p class="nome">Celta</p>
+            <p class="nome">Classic Sedan</p>
           </div>
 
           <div class="pin">
             <img src="dist/img/carros/toro.jpg" alt="" class="img" />
-            <h3 class="grupo">Grupo A</h3>
-            <p class="nome">Celta</p>
+            <h3 class="grupo">Grupo E</h3>
+            <p class="nome">Fiat Toro</p>
           </div>
 
           <div class="pin">
             <img src="dist/img/carros/bmw.jpg" alt="" class="img" />
-            <h3 class="grupo">Grupo A</h3>
-            <p class="nome">Celta</p>
+            <h3 class="grupo">Grupo F</h3>
+            <p class="nome">BMW M6</p>
           </div>
 
           <div class="pin">
             <img src="dist/img/carros/hilux.jpg" alt="" class="img" />
-            <h3 class="grupo">Grupo A</h3>
-            <p class="nome">Celta</p>
+            <h3 class="grupo">Grupo G</h3>
+            <p class="nome">Hilux</p>
           </div>
 
           <div class="pin">
             <img src="dist/img/carros/corolla.jpg" alt="" class="img" />
-            <h3 class="grupo">Grupo A</h3>
-            <p class="nome">Celta</p>
+            <h3 class="grupo">Grupo D</h3>
+            <p class="nome">Corolla</p>
           </div>
 
           <div class="pin">
             <img src="dist/img/carros/corsa.jpg" alt="" class="img" />
             <h3 class="grupo">Grupo A</h3>
-            <p class="nome">Celta</p>
+            <p class="nome">Corsa</p>
           </div>
 
           <div class="pin">
             <img src="dist/img/carros/doblo.jpg" alt="" class="img" />
-            <h3 class="grupo">Grupo A</h3>
-            <p class="nome">Celta</p>
+            <h3 class="grupo">Grupo B</h3>
+            <p class="nome">Doblo</p>
           </div>
 
           <div class="pin">
             <img src="dist/img/carros/hyundai_hb20_1.png" alt="" class="img" />
-            <h3 class="grupo">Grupo A</h3>
-            <p class="nome">Celta</p>
+            <h3 class="grupo">Grupo C</h3>
+            <p class="nome">Hyundai HB20</p>
           </div>
 
           <div class="pin">
             <img src="dist/img/carros/ix35.jpg" alt="" class="img" />
-            <h3 class="grupo">Grupo A</h3>
-            <p class="nome">Celta</p>
+            <h3 class="grupo">Grupo C</h3>
+            <p class="nome">IX35</p>
           </div>
 
           <div class="pin">
             <img src="dist/img/carros/l200.jpg" alt="" class="img" />
-            <h3 class="grupo">Grupo A</h3>
-            <p class="nome">Celta</p>
+            <h3 class="grupo">Grupo G</h3>
+            <p class="nome">Mitsubishi L200</p>
           </div>
 
           <div class="pin">
             <img src="dist/img/carros/land-rover.jpg" alt="" class="img" />
-            <h3 class="grupo">Grupo A</h3>
-            <p class="nome">Celta</p>
+            <h3 class="grupo">Grupo F</h3>
+            <p class="nome">Land Rover Evoque</p>
           </div>
 
           <div class="pin">
             <img src="dist/img/carros/montana.jpg" alt="" class="img" />
-            <h3 class="grupo">Grupo A</h3>
-            <p class="nome">Celta</p>
+            <h3 class="grupo">Grupo E</h3>
+            <p class="nome">Montana</p>
           </div>
 
           <div class="pin">
             <img src="dist/img/carros/novo-fiesta.jpg" alt="" class="img" />
-            <h3 class="grupo">Grupo A</h3>
-            <p class="nome">Celta</p>
+            <h3 class="grupo">Grupo C</h3>
+            <p class="nome">Novo Fiesta</p>
           </div>
 
           <div class="pin">
             <img src="dist/img/carros/Onix.png" alt="" class="img" />
-            <h3 class="grupo">Grupo A</h3>
-            <p class="nome">Celta</p>
+            <h3 class="grupo">Grupo C</h3>
+            <p class="nome">Onix</p>
           </div>
 
           <div class="pin">
             <img src="dist/img/carros/Seguro-auto-Toyota-Corolla.png" alt="" class="img" />
-            <h3 class="grupo">Grupo A</h3>
-            <p class="nome">Celta</p>
+            <h3 class="grupo">Grupo D</h3>
+            <p class="nome">Corolla</p>
           </div>
         </div>
       </div>
